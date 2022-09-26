a3be2958b94b4713490f3336f5e1d884016ade23
Roman Riepa <pwroma@gmail.com>
2022-09-26 11:55:17 +0300

    final technical part

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var input = 100.0
-    @State private var inputUnit = UnitLength.meters
-    @State private var outputUnit = UnitLength.kilometers
+    @State private var selectedUnits = 0
+    @State private var inputUnit: Dimension = UnitLength.meters
+    @State private var outputUnit: Dimension = UnitLength.kilometers
     @FocusState private var inputIsFocused: Bool
     
-    let units: [UnitLength] = [.feet, .kilometers, .meters, .miles, .yards, .millimeters]
+    let conversions = ["Distance", "Mass", "Temperature", "Time"]
+    
+    let unitTypes = [
+        [UnitLength.feet, UnitLength.kilometers, UnitLength.meters, UnitLength.yards, UnitLength.millimeters, UnitLength.miles],
+        [UnitMass.grams, UnitMass.kilograms, UnitMass.ounces, UnitMass.pounds],
+        [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin],
+        [UnitDuration.hours, UnitDuration.minutes, UnitDuration.seconds]
+    ]
     
     let formatter: MeasurementFormatter
     
@@ -33,13 +41,20 @@ struct ContentView: View {
                 } header: {
                     Text("Amount to convert")
                 }
+                
+                Picker("Conversion", selection: $selectedUnits) {
+                    ForEach(0..<conversions.count) {
+                        Text(conversions[$0])
+                    }
+                }
+                
                 Picker("Convert from", selection: $inputUnit) {
-                    ForEach(units, id: \.self) {
+                    ForEach(unitTypes[selectedUnits], id: \.self) {
                         Text(formatter.string(from: $0).capitalized)
                     }
                 }
                 Picker("Convert to", selection: $outputUnit) {
-                    ForEach(units, id: \.self) {
+                    ForEach(unitTypes[selectedUnits], id: \.self) {
                         Text(formatter.string(from: $0).capitalized)
                     }
                 }
@@ -58,6 +73,11 @@ struct ContentView: View {
                         inputIsFocused = false
                     }
                 }
+            }
+            .onChange(of: selectedUnits) { newSelection in
+                let units = unitTypes[newSelection]
+                inputUnit = units[0]
+                outputUnit = units[1]
             }
         }
     }
